9d6445ba053e1fc60edad36863b7e5e4d2405169
Sipalaki <1034118854@qq.com>
2022-05-30 09:42:53 +0800

    Update main.c

--- Drivers/USER/main.c
+++ Drivers/USER/main.c
@@ -15,35 +15,35 @@
   * @retval None
   */
 	
-//任务优先级
+//?????????
 #define START_TASK_PRIO		3
-//任务堆栈大小	
+//?????????	
 #define START_STK_SIZE 		64
-//任务控制块
+//????????
 OS_TCB StartTaskTCB;
-//任务堆栈	
+//??????	
 CPU_STK START_TASK_STK[START_STK_SIZE];
-//任务函数
+//??????
 void start_task(void *p_arg);
 
 
-//任务优先级
+//?????????
 #define OLED_TASK_PRIO		4
-//任务堆栈大小	
+//?????????	
 #define OLED_STK_SIZE 		128
-//任务控制块
+//????????
 OS_TCB OLEDTaskTCB;
-//任务堆栈	
+//??????	
 CPU_STK OLED_TASK_STK[OLED_STK_SIZE];
 void oled_task(void *p_arg);
 
-//任务优先级
+//?????????
 #define CTR_TASK_PRIO		4
-//任务堆栈大小	
+//?????????	
 #define CTR_STK_SIZE 		64
-//任务控制块
+//????????
 OS_TCB CTRTaskTCB;
-//任务堆栈	
+//??????	
 CPU_STK CTR_TASK_STK[CTR_STK_SIZE];
 void ctr_task(void *p_arg);
 
@@ -63,33 +63,33 @@ int main(void)
 	CPU_SR_ALLOC();
 	
 	systemdelay_init();
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //中断分组配置
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //??????????
 	I2C_Configuration();
 	LED_Init();
-	init(&q_x,16);
-	init(&q_y,16);
+	init(&q_x,24);
+	init(&q_y,24);
 	ADC_Configuration(); 
 	JoyStick_Init(); 
-	uart_init(115200);    //串口波特率设置
-	OSInit(&err);		//初始化UCOSIII
-	OS_CRITICAL_ENTER();//进入临界区
-	//创建开始任务
-	OSTaskCreate((OS_TCB 	* )&StartTaskTCB,		//任务控制块
-				 (CPU_CHAR	* )"start task", 		//任务名字
-                 (OS_TASK_PTR )start_task, 			//任务函数
-                 (void		* )0,					//传递给任务函数的参数
-                 (OS_PRIO	  )START_TASK_PRIO,     //任务优先级
-                 (CPU_STK   * )&START_TASK_STK[0],	//任务堆栈基地址
-                 (CPU_STK_SIZE)START_STK_SIZE/10,	//任务堆栈深度限位
-                 (CPU_STK_SIZE)START_STK_SIZE,		//任务堆栈大小
-                 (OS_MSG_QTY  )0,					//任务内部消息队列能够接收的最大消息数目,为0时禁止接收消息
-                 (OS_TICK	  )0,					//当使能时间片轮转时的时间片长度，为0时为默认长度，
-                 (void   	* )0,					//用户补充的存储区
-                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR, //任务选项
-                 (OS_ERR 	* )&err);				//存放该函数错误时的返回值
+	uart_init(115200);    //?????????????
+	OSInit(&err);		//??'??UCOSIII
+	OS_CRITICAL_ENTER();//?????????
+	//??????'????
+	OSTaskCreate((OS_TCB 	* )&StartTaskTCB,		//????????
+				 (CPU_CHAR	* )"start task", 		//????????
+                 (OS_TASK_PTR )start_task, 			//??????
+                 (void		* )0,					//????????????????
+                 (OS_PRIO	  )START_TASK_PRIO,     //?????????
+                 (CPU_STK   * )&START_TASK_STK[0],	//???????????
+                 (CPU_STK_SIZE)START_STK_SIZE/10,	//????????????
+                 (CPU_STK_SIZE)START_STK_SIZE,		//?????????
+                 (OS_MSG_QTY  )0,					//???????????????????????????????,?0???????????
+                 (OS_TICK	  )0,					//??'??????????????????????0??I??????
+                 (void   	* )0,					//?没?????J???
+                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR, //???????
+                 (OS_ERR 	* )&err);				//??鸥煤?????????k????
 //								 printf("%d",err);
-	OS_CRITICAL_EXIT();	//退出临界区	 
-	OSStart(&err);  //开启UCOSIII
+	OS_CRITICAL_EXIT();	//????????	 
+	OSStart(&err);  //????UCOSIII
 
   /* Infinite loop */
   while (1);
@@ -98,7 +98,7 @@ int main(void)
 	
 }
 
-//开始任务函数
+//??'??????
 void start_task(void *p_arg)
 {
 	OS_ERR err;
@@ -107,20 +107,20 @@ void start_task(void *p_arg)
 
 	CPU_Init();
 #if OS_CFG_STAT_TASK_EN > 0u
-   OSStatTaskCPUUsageInit(&err);  	//统计任务                
+   OSStatTaskCPUUsageInit(&err);  	//???????                
 #endif
 	
-#ifdef CPU_CFG_INT_DIS_MEAS_EN		//如果使能了测量中断关闭时间
+#ifdef CPU_CFG_INT_DIS_MEAS_EN		//???'???????????????
     CPU_IntDisMeasMaxCurReset();	
 #endif
 	
-#if	OS_CFG_SCHED_ROUND_ROBIN_EN  //当使用时间片轮转的时候
-	 //使能时间片轮转调度功能,时间片长度为1个系统时钟节拍，既1*5=5ms
+#if	OS_CFG_SCHED_ROUND_ROBIN_EN  //??'??????????????
+	 //'????????????????,?????????1?????????g???1*5=5ms
 	OSSchedRoundRobinCfg(DEF_ENABLED,1,&err);  
 #endif		
 	
-	OS_CRITICAL_ENTER();	//进入临界区
-	//创建LED0任务
+	OS_CRITICAL_ENTER();	//?????????
+	//????LED0????
 								 
 	OSTaskCreate((OS_TCB 	* )&OLEDTaskTCB,		
 				 (CPU_CHAR	* )"oled task", 		
@@ -151,8 +151,8 @@ void start_task(void *p_arg)
                  (OS_ERR 	* )&err);		
 
 			 						 
-	OS_TaskSuspend((OS_TCB*)&StartTaskTCB,&err);		//挂起开始任务			 
-	OS_CRITICAL_EXIT();	//进入临界区
+	OS_TaskSuspend((OS_TCB*)&StartTaskTCB,&err);		//????'????			 
+	OS_CRITICAL_EXIT();	//?????????
 }
 
 
@@ -179,13 +179,13 @@ void oled_task(void *p_arg)
 	rand_y = rand()%8;
 	while(1)
 	{
-		OSTimeDlyHMSM(0,0,0,200,OS_OPT_TIME_HMSM_STRICT,&err); //延时500ms    
+		OSTimeDlyHMSM(0,0,0,200,OS_OPT_TIME_HMSM_STRICT,&err); //???500ms    
 		if(flag){
 		OLED_DISPLAY_8x8(rand_y,rand_x*8,2);
 		switch(vector){
 		case 0:
 			x = q_x.rear;
-			if(x - 1 < 0){x = 16;} else {x = x-1;}
+			if(x - 1 < 0){x = q_x.capacity;} else {x = x-1;}
 			if(q_x.data[x]+8>127)
 				{ enQueue(&q_x,0);
 					OLED_DISPLAY_8x8(q_y.data[x],0,0);
@@ -202,7 +202,7 @@ void oled_task(void *p_arg)
 			break;
 		case 1:
 			x = q_x.rear;
-			if(x - 1 < 0){x = 16;} else {x = x-1;}
+			if(x - 1 < 0){x = q_x.capacity;} else {x = x-1;}
 			if(q_x.data[x]-8<0)
 				{ enQueue(&q_x,120);
 					OLED_DISPLAY_8x8(q_y.data[x],120,0);
@@ -220,7 +220,7 @@ void oled_task(void *p_arg)
 			break;
 		case 2:
 			x = q_x.rear;
-			if(x - 1 < 0){x = 16;} else {x = x-1;}
+			if(x - 1 < 0){x = q_x.capacity;} else {x = x-1;}
 			if(q_y.data[x]-1<0)
 				{ enQueue(&q_y,7);
 					OLED_DISPLAY_8x8(7,q_x.data[x],0);
@@ -237,7 +237,7 @@ void oled_task(void *p_arg)
 			break;
 		case 3:
 			x = q_x.rear;
-			if(x - 1 < 0){x = 16;} else {x = x-1;}
+			if(x - 1 < 0){x = q_x.capacity;} else {x = x-1;}
 			if(q_y.data[x]+1>7)
 				{ enQueue(&q_y,0);
 					OLED_DISPLAY_8x8(0,q_x.data[x],0);
@@ -300,7 +300,7 @@ void ctr_task(void *p_arg)
 	p_arg = p_arg;
 	while(1)
 	{
-		OSTimeDlyHMSM(0,0,0,50,OS_OPT_TIME_HMSM_STRICT,&err); //延时500ms
+		OSTimeDlyHMSM(0,0,0,50,OS_OPT_TIME_HMSM_STRICT,&err); //???500ms
 		if(ADC_DMA_IN[0]>3000){
 				vector = 0;
 		}else if(ADC_DMA_IN[0]<1000){
@@ -312,5 +312,4 @@ void ctr_task(void *p_arg)
 		}
 	}
 }
-
 
